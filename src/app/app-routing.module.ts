84217baaac843d86d74dafc3d55a49025dc75f5e
beshoCRM <bfanous.crm@gmail.com>
2024-08-21 20:03:35 +0300

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Pages/home/home.component';
+import { ReservationFormComponent } from './Pages/reservation-form/reservation-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '*',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'reservation',
+    component: ReservationFormComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
